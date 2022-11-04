9e3a3185b9f06ecae13973beedc208d2c815fe41
FlorentEstur <warcr@DESKTOP-3VUL9G4.home>
2022-11-04 09:43:53 +0100

    Version de base

---
+++ src/main/java/com/inti/Td3Ex2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td3Ex2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td3Ex2SpringApplication.class, args);
+	}
+
+}
