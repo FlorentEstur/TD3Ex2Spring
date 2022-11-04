e6d631d01d27ddbf607d47dd4ae2d2c857c88b6c
FlorentEstur <warcr@DESKTOP-3VUL9G4.home>
2022-11-04 10:49:49 +0100

    ajout des repository

--- src/main/java/com/inti/services/IProfesseurRepository.java
+++ src/main/java/com/inti/services/IProfesseurRepository.java
@@ -4,12 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.inti.model.Ecole;
+import com.inti.model.Professeur;
 
 @Repository
-public interface IEcoleRepository extends JpaRepository<Ecole, Integer> {
-	
-	@Query(value = "select max(id) from ecole_TD3", nativeQuery = true)
-	int findMaxId();
+public interface IProfesseurRepository extends JpaRepository<Professeur, Integer> {
 
+	@Query(value = "select max(id) from professeur_TD3", nativeQuery = true)
+	int findMaxId();
 }
